6446d83efb04cdf914565c7f815cd517eaded6ba
Serhat Diyaroğlu <serhatd1917@gmail.com>
2023-12-14 10:26:15 +0300

    deneme

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,7 @@ public class Main {
 
          Scanner input = new Scanner(System.in);
 
-         int a,b;
-         double c;
 
-        System.out.println("birinci kenar :");
-        a = input.nextInt();
-        System.out.println("ikinci kenar : ");
-        b = input.nextInt();
-
-        c = Math.sqrt((a*a)+(b*b));
-        System.out.println(c);
 
 
         }
